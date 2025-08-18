3feb475104b49c7ea6281cdb01bdb5f696dd6ec2
khanhtv <trinhvukhanh277@gmail.com>
2025-08-18 21:16:02 +0700

    setting yaml

---
+++ src/main/java/spring/backend/m_bl5_g1_su25/OnlineShopping/ProductScreen/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package spring.backend.m_bl5_g1_su25.OnlineShopping.ProductScreen.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import spring.backend.m_bl5_g1_su25.OnlineShopping.AuthorizedScreen.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
