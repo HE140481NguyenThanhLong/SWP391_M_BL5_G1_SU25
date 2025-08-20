9ed762b11ed9868faf890080e6aaad6edcc1ecf4
Lê Thanh Lâm-HE140605 <lamlthe140605@fpt.edu.vn>
2025-08-20 18:24:18 +0700

    commit thiếu

--- src/main/java/spring/backend/m_bl5_g1_su25/OnlineShopping/AuthorizedScreen/service/AuthorizedService.java
+++ src/main/java/spring/backend/m_bl5_g1_su25/OnlineShopping/AuthorizedScreen/service/AuthorizedService.java
@@ -2,11 +2,7 @@ package spring.backend.m_bl5_g1_su25.OnlineShopping.AuthorizedScreen.service;
 
 import spring.backend.m_bl5_g1_su25.OnlineShopping.AuthorizedScreen.dto.request.SignUpRequest;
 import spring.backend.m_bl5_g1_su25.OnlineShopping.UserScreen.entity.Customer;
-import spring.backend.m_bl5_g1_su25.OnlineShopping.UserScreen.entity.User;
 
 public interface AuthorizedService {
     Customer signUp(SignUpRequest request);
-    User login(String email, String password);
-
-
 }
