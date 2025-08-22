7c9957b2b9c7b2cfe8947d04c2833ef0eb8f0a80
HE140481NguyenThanhLong <longnthe140481@fpt.edu.vn>
2025-08-22 07:22:06 +0700

    init favorite logic handling for list favorite product and inject thymealeaf to right url

---
+++ src/main/java/spring/backend/m_bl5_g1_su25/OnlineShopping/ProductScreen/controller/FavoriteProductController.java
@@ -0,0 +1,37 @@
+package spring.backend.m_bl5_g1_su25.OnlineShopping.ProductScreen.controller;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import spring.backend.m_bl5_g1_su25.OnlineShopping.ProductScreen.entity.Product;
+import spring.backend.m_bl5_g1_su25.OnlineShopping.ProductScreen.repository.FavoriteRepository;
+import spring.backend.m_bl5_g1_su25.OnlineShopping.ProductScreen.service.FavoriteService;
+import spring.backend.m_bl5_g1_su25.OnlineShopping.UserScreen.entity.User;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/favoriteScreen")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class FavoriteProductController {
+    @Autowired
+    FavoriteService favoriteService;
+
+    @GetMapping("/favorite")
+    public String favoriteProducts(Model model, HttpSession session) {
+        User loggedInUser = (User) session.getAttribute("loggedInUser");
+        List<Product> favoriteProducts = favoriteService.getFavoriteProducts(loggedInUser.getUsername());
+        model.addAttribute("favoriteProducts", favoriteProducts);
+        if (favoriteProducts.isEmpty()) {
+            model.addAttribute("message", "You don't have any favorite products");
+        }
+        return "/favoriteScreen/favorite";
+
+    }
+
+}
