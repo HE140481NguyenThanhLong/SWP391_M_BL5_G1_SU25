757831983700af72f7b97f558014adfdd11b373b
Trinh Vu Khanh <trinhvukhanh277@gmail.com>
2025-08-28 06:27:51 +0700

    Update PaymentController.java
    fix payment price

--- src/main/java/spring/backend/m_bl5_g1_su25/OnlineShopping/PaymentScreen/controller/PaymentController.java
+++ src/main/java/spring/backend/m_bl5_g1_su25/OnlineShopping/PaymentScreen/controller/PaymentController.java
@@ -23,6 +23,7 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
 
+
 @Controller
 @RequestMapping("/payment")
 @RequiredArgsConstructor
@@ -32,7 +33,6 @@ public class PaymentController {
     private final PaymentRepository paymentRepository;
     private final TransactionRepository transactionRepository;
     private final OrderRepository orderRepository;
-
 
     @GetMapping("")
     public String home(Model model,
@@ -44,34 +44,39 @@ public class PaymentController {
         if (loggedInUser == null) return "redirect:/authority/signin";
         int userId = loggedInUser.getUser_id();
 
-
         Order order = orderRepository.findById(orderId).orElseThrow(() -> new RuntimeException("Không tìm thấy đơn hàng cần thực hiện"));
         Payment payment = paymentRepository.findByOrderId(orderId).orElseThrow(() -> new RuntimeException("Không tìm thấy thanh toán cần thực hiện"));
         List<String> cardNumbers = transactionRepository.findCardNumbers(userId);
 
         Transaction trans = transactionRepository.findTopByUserIdAndDate(userId).orElse(null);
 
-        if(cardNumbers != null || !cardNumbers.isEmpty()) {
+        if(cardNumbers != null && !cardNumbers.isEmpty()) {
             trans = transactionRepository.findTopByUserIdAndCardNumber(userId, cardNumber).orElse(null);
         }
-
 
         model.addAttribute("trans", trans);
         model.addAttribute("payment", payment);
         model.addAttribute("order", order);
         model.addAttribute("cardNumbers", cardNumbers);
 
+        // Tính toán giá trị của giỏ hàng
 
-// Tính toán giá trị của giỏ hàng
-        BigDecimal total = BigDecimal.valueOf(order.getTotal());
-        BigDecimal savings = BigDecimal.ZERO; // chỗ này có thể sau này tính mã giảm giá
-        BigDecimal storePickup = BigDecimal.ZERO; // phí lấy tại cửa hàng (nếu có)
-        BigDecimal originalPrice = total.divide(new BigDecimal("1.08"), 2, RoundingMode.HALF_UP); // thuế
+//        BigDecimal total = BigDecimal.valueOf(order.getTotal());
+//        BigDecimal savings = BigDecimal.ZERO; // chỗ này có thể sau này tính mã giảm giá
+//        BigDecimal storePickup = BigDecimal.ZERO; // phí lấy tại cửa hàng
+//        BigDecimal originalPrice = total.divide(new BigDecimal("1.08"), 2, RoundingMode.HALF_UP); // thuế
+
+        BigDecimal originalPrice = BigDecimal.valueOf(order.getTotal());
+        BigDecimal savings = BigDecimal.ZERO;
+        BigDecimal storePickup = BigDecimal.ZERO;
+        BigDecimal tax = originalPrice.multiply(new BigDecimal("0.08"));
+        BigDecimal total = originalPrice.add(tax);
+
 
         model.addAttribute("originalPrice", originalPrice);
         model.addAttribute("savings", savings);
         model.addAttribute("storePickup", storePickup);
-        model.addAttribute("tax", total.subtract(originalPrice));
+        model.addAttribute("tax", tax);
         model.addAttribute("total", total);
         return "payment/payment";
     }
